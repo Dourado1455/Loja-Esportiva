d00e22bcb700ce88c4817d3ef82819b83a56fd26
Dourado1455 <144744940+Dourado1455@users.noreply.github.com>
2024-11-30 23:00:29 -0300

    Primeiro commit: adicionando formulário de solicitação de férias

--- loja_esportiva/loja/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Produto
-
-# View para listar os produtos
-def listar_produtos(request):
-    produtos = Produto.objects.all()  # Obtém todos os produtos cadastrados
-    return render(request, 'loja/listar_produtos.html', {'produtos': produtos})
